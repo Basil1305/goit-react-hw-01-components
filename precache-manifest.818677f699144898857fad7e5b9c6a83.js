639c18b863a30c42f8099edfb7098fb4402384cc
Basil Koval <lodupu8989@g,gmail.com>
2020-06-10 00:17:45 +0300

    Updates

---
+++ precache-manifest.818677f699144898857fad7e5b9c6a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c3369beb7723d35a162fcb54d95d2e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "979f2d9829ff2fc3c627",
+    "url": "/goit-react-hw-01-components/static/css/main.f64a3e09.chunk.css"
+  },
+  {
+    "revision": "b4254a951c4e5f6e185c",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979f2d9829ff2fc3c627",
+    "url": "/goit-react-hw-01-components/static/js/main.3cc04b4f.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
