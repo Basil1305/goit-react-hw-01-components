0674d4f05fe2b1d01bc7b2248fe96d94dcc977c8
Basil Koval <lodupu8989@g,gmail.com>
2020-06-10 16:52:00 +0300

    Updates

--- precache-manifest.d683a43c5bed1bce35bf6fc7c8248ad1.js
+++ precache-manifest.d683a43c5bed1bce35bf6fc7c8248ad1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0c3369beb7723d35a162fcb54d95d2e",
+    "revision": "5bc08a12b4669faa4349ec1c0386c514",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "979f2d9829ff2fc3c627",
-    "url": "/goit-react-hw-01-components/static/css/main.f64a3e09.chunk.css"
+    "revision": "c2a5ef4ef2f2a2b0b307",
+    "url": "/goit-react-hw-01-components/static/css/main.9b134c1c.chunk.css"
   },
   {
     "revision": "b4254a951c4e5f6e185c",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "979f2d9829ff2fc3c627",
-    "url": "/goit-react-hw-01-components/static/js/main.3cc04b4f.chunk.js"
+    "revision": "c2a5ef4ef2f2a2b0b307",
+    "url": "/goit-react-hw-01-components/static/js/main.58e6d345.chunk.js"
   },
   {
     "revision": "cf184cad0926235fce4a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.818677f699144898857fad7e5b9c6a83.js"
+  "/goit-react-hw-01-components/precache-manifest.d683a43c5bed1bce35bf6fc7c8248ad1.js"
 );
 
 self.addEventListener('message', (event) => {
